5625a4120548bdab82cdaca49a2b1b24ff1b1eb6
Hang Shi <hangshiisi@gmail.com>
2016-03-11 22:03:35 -0800

    bigc1

---
+++ gotest/bigc1.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/websocket"
+	"net/http"
+)
+
+func main() { 
+	var sock = new WebSocket("ws://localhost:4000/")
+	sock.onmessage = function(m) { console.log("Received:", m.data) }
+	sock.send("Hello!\n")
+
+} 
